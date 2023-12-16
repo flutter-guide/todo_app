93aafada0898e1a63c5d389989475bb3e3b018f5
Julius Stümmler <julius.stuemmler@gmx.de>
2023-12-16 21:01:36 +0100

    introduced separation for pending and finished tasks

--- lib/view/todo/todo_page.dart
+++ lib/view/todo/todo_page.dart
@@ -2,14 +2,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/domain/firebase/datastore/todo_api.dart';
 import 'package:todo_app/domain/models/todo.dart';
+import 'package:todo_app/view/todo/widgets/finished_tasks_button.dart';
 import 'package:todo_app/view/todo/widgets/todo_card.dart';
 
 import '../../data/todo/todo_repository.dart';
 
-class ToDoPage extends StatelessWidget {
+class ToDoPage extends StatefulWidget {
   ToDoPage({super.key});
 
+  @override
+  State<ToDoPage> createState() => _ToDoPageState();
+}
+
+class _ToDoPageState extends State<ToDoPage> {
   final TodoRepository todoRepository = TodoRepository(TodoApi());
+
+  bool showPendingTasks = true;
+  bool showFinishedTasks = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +29,60 @@ class ToDoPage extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.active &&
               snapshot.hasData) {
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                Todo todo = snapshot.data!.elementAt(index);
-                return TodoCard(
-                  todo: todo,
-                  todoRepository: todoRepository,
-                );
-              },
-              itemCount: snapshot.data!.length,
+            return ListView(
+              children: [
+                ToggleTaskSectionButton(
+                  title: "Pending Tasks",
+                  backgroundColor: Colors.amber,
+                  showFinishedTasks: showPendingTasks,
+                  toggle: () {
+                    setState(() => showPendingTasks = !showPendingTasks);
+                  },
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                if (snapshot.data!.where((element) => !element.done).isEmpty &&
+                    showPendingTasks)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Center(
+                      child: Text(
+                        "No pending tasks found!",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                if (showPendingTasks)
+                  for (final todo
+                      in snapshot.data!.where((element) => !element.done))
+                    TodoCard(
+                      todo: todo,
+                      todoRepository: todoRepository,
+                    ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ToggleTaskSectionButton(
+                  title: "Finished Tasks",
+                  showFinishedTasks: showFinishedTasks,
+                  toggle: () {
+                    setState(() => showFinishedTasks = !showFinishedTasks);
+                  },
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                if (showFinishedTasks)
+                  for (final todo
+                      in snapshot.data!.where((element) => element.done))
+                    TodoCard(
+                      todo: todo,
+                      todoRepository: todoRepository,
+                    ),
+              ],
             );
           } else if (!snapshot.hasData &&
               snapshot.connectionState == ConnectionState.waiting) {
